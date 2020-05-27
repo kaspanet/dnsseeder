41009cdc4a058e74e42a47650ad83885fd6ba065
stasatdaglabs <stas@daglabs.com>
2020-05-27 16:35:05 +0300

    [NOD-932] Add a log.

--- dnsseed.go
+++ dnsseed.go
@@ -120,6 +120,7 @@ func creep() {
 				if err != nil {
 					log.Warnf(err.Error())
 					if defaultSeeder != nil && addr == defaultSeeder {
+						log.Criticalf("Failed to poll default seeder. Exiting...")
 						os.Exit(1)
 					}
 				}
