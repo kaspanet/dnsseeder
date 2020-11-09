4a7b8975f4e6a188d61581332ee7d9a8f271c4b5
Mike Zak <feanorr@gmail.com>
2020-11-09 11:16:15 +0200

    [NOD-1522] Update versions properly

--- version/version.go
+++ version/version.go
@@ -10,8 +10,8 @@ const validCharacters = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrs
 
 const (
 	appMajor uint = 0
-	appMinor uint = 7
-	appPatch uint = 3
+	appMinor uint = 8
+	appPatch uint = 1
 )
 
 // appBuild is defined as a variable so it can be overridden during the build
