785cd1fecae3179ea3c9b6afe678cd00b6ae7943
Tiram <18632023+tiram88@users.noreply.github.com>
2023-06-19 01:02:25 +0200

    Add support for testnet-11

--- config.go
+++ config.go
@@ -54,6 +54,7 @@ type ConfigFlags struct {
 	Seeder      string `short:"s" long:"default-seeder" description:"IP address of a working node, optionally with a port specifier"`
 	Profile     string `long:"profile" description:"Enable HTTP profiling on given port -- NOTE port must be between 1024 and 65536"`
 	GRPCListen  string `long:"grpclisten" description:"Listen gRPC requests on address:port"`
+	NetSuffix   uint16 `long:"netsuffix" description:"Testnet network suffix number"`
 	config.NetworkFlags
 }
 
@@ -100,6 +101,7 @@ func loadConfig() (*ConfigFlags, error) {
 		AppDir:     DefaultAppDir,
 		Listen:     normalizeAddress("localhost", defaultListenPort),
 		GRPCListen: normalizeAddress("localhost", defaultGrpcListenPort),
+		NetSuffix:  10,
 	}
 
 	preCfg := activeConfig
@@ -165,6 +167,19 @@ func loadConfig() (*ConfigFlags, error) {
 	err = activeConfig.ResolveNetwork(parser)
 	if err != nil {
 		return nil, err
+	}
+
+	// Manually enforce testnet 11 net params so we do not have to 
+	// support this special network in kaspad.
+	if activeConfig.NetSuffix != 0 && !activeConfig.Testnet {
+		return nil, errors.New("The net suffix can only be used with testnet")
+	}
+	if activeConfig.NetSuffix != 11 && activeConfig.Testnet {
+		return nil, errors.New("The only supported explicit testnet net suffix is 11")
+	}
+	if activeConfig.NetSuffix == 11 && activeConfig.Testnet {
+		activeConfig.NetParams().DefaultPort = "16311";
+		activeConfig.NetParams().Name = "kaspa-testnet-11";
 	}
 
 	activeConfig.AppDir = cleanAndExpandPath(activeConfig.AppDir)
