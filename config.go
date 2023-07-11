51aa55578de0729ce164491b7442ce32d89a0230
Marius Thøring <mthoring@gmail.com>
2023-07-12 00:36:16 +0200

    Support disabling of file logging (--nologfiles)

--- config.go
+++ config.go
@@ -55,6 +55,7 @@ type ConfigFlags struct {
 	Profile     string `long:"profile" description:"Enable HTTP profiling on given port -- NOTE port must be between 1024 and 65536"`
 	GRPCListen  string `long:"grpclisten" description:"Listen gRPC requests on address:port"`
 	NetSuffix   uint16 `long:"netsuffix" description:"Testnet network suffix number"`
+	NoLogFiles  bool   `long:"nologfiles" description:"Disable logging to file"`
 	config.NetworkFlags
 }
 
@@ -204,7 +205,7 @@ func loadConfig() (*ConfigFlags, error) {
 		}
 	}
 
-	initLog(appLogFile, appErrLogFile)
+	initLog(activeConfig.NoLogFiles, appLogFile, appErrLogFile)
 
 	return activeConfig, nil
 }
--- log.go
+++ log.go
@@ -14,22 +14,24 @@ var (
 	spawn      = panics.GoroutineWrapperFunc(log)
 )
 
-func initLog(logFile, errLogFile string) {
-	err := backendLog.AddLogFile(logFile, logger.LevelTrace)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error adding log file %s as log rotator for level %s: %s", logFile, logger.LevelTrace, err)
-		os.Exit(1)
-	}
-	err = backendLog.AddLogFile(errLogFile, logger.LevelWarn)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error adding log file %s as log rotator for level %s: %s", errLogFile, logger.LevelWarn, err)
-		os.Exit(1)
-	}
-
-	err = backendLog.AddLogWriter(os.Stdout, logger.LevelInfo)
+func initLog(noLogFiles bool, logFile, errLogFile string) {
+	err := backendLog.AddLogWriter(os.Stdout, logger.LevelInfo)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error adding stdout to the loggerfor level %s: %s", logger.LevelWarn, err)
 		os.Exit(1)
+	}
+
+	if !noLogFiles {
+		err = backendLog.AddLogFile(logFile, logger.LevelTrace)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error adding log file %s as log rotator for level %s: %s", logFile, logger.LevelTrace, err)
+			os.Exit(1)
+		}
+		err = backendLog.AddLogFile(errLogFile, logger.LevelWarn)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error adding log file %s as log rotator for level %s: %s", errLogFile, logger.LevelWarn, err)
+			os.Exit(1)
+		}
 	}
 
 	err = backendLog.Run()
